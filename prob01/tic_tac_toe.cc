14cfbdd7c2f914cc4b3e2592a597edbdb82dd6a5
Joanna Cruz <joannacruz@csu.fullerton.edu>
2021-09-15 18:57:36 -0700

    first commit

--- prob01/tic_tac_toe.cc
+++ prob01/tic_tac_toe.cc
@@ -6,4 +6,39 @@ int CheckGame(std::vector<std::vector<int>>& game) {
   // Your code here to determine if there was a winner, a draw, or if the
   // game can continue.
 
+
+int main()
+{
+
+ vector<int> numbers = {-1, 0, 1, 2};
+  
+  const int SIZE = 4
+  
+  // define vector to hold 4 int values
+  vector<int> numbers(SIZE);
+  
+ for (int x = 0; x < game.size(); x++) {
+   for (int y = 0; game.size(); y++) {
+   x = game.size; 
+   break;
+   
+   } 
+
+}
+  
+ 
+ 
+  // store values 0 through 2 in the vector  
+  for (int index = -1; index < numbers.size(); index++)
+    numbers [index] = index;
+  
+  // display vector element  
+  for (auto element : numbers)
+    cout << element << " ";
+  cout << endl; 
+  
+  
+  return result; 
+
+
 }
